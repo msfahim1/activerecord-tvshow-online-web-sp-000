c96b047c7236ac9e68904464ae989fecb8ea2c77
Michael Fahim <michael_fahim@yahoo.com>
2020-04-24 00:08:04 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,8 @@
-class CreateShows < ActiveRecord::Migration
-  def change
-    create_table :shows do |t|
-      t.string :name
-      t.string :network
-      t.string :day
-      t.integer :rating
-    end
+class CreateShows < ActiveRecord::Migration[5.2]
+  create_table :shows do |t|
+    t.string :name
+    t.string :network
+    t.string :day
+    t.integer :rating
   end
 end
